b7042a2845a754debf6d0a956d47a72d4d8f39dd
duckpool <duckpool@yandex.ru>
2021-08-31 15:42:29 +0300

    Initial commit

---
+++ src/main/java/ru/geekbrains/shop/GbShopApplication.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GbShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GbShopApplication.class, args);
+	}
+
+}
