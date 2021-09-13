68eacd0a731e0b04ef026de44176946850cf3a9c
duckpool <duckpool@yandex.ru>
2021-09-13 17:42:14 +0300

    Insert data in the tables

--- src/main/resources/db/migration/V1.0.1__create_User_Role_tables.sql
+++ src/main/resources/db/migration/V1.0.1__create_User_Role_tables.sql
@@ -1,4 +1,4 @@
-create table users
+create table user_data
 (
     id       bigserial primary key,
     username varchar(30) not null unique,
@@ -7,7 +7,7 @@ create table users
     active   boolean default true
 );
 
-create table role
+create table role_data
 (
     id   serial primary key,
     name varchar(50) not null
@@ -18,6 +18,23 @@ CREATE TABLE user_role
     user_id bigint not null,
     role_id int    not null,
     primary key (user_id, role_id),
-    foreign key (user_id) references users (id),
-    foreign key (role_id) references role (id)
+    foreign key (user_id) references user_data (id),
+    foreign key (role_id) references role_data (id)
 );
+insert into role_data (name)
+values ('ROLE_USER'),
+       ('ROLE_MANAGER'),
+       ('ROLE_ADMIN');
+
+insert into user_data (username, password, email)
+values ('admin', 'admin', 'user@gmail.com');
+
+insert into user_role (user_id, role_id)
+values (1, 1),
+       (1, 2);
+
+create
+extension if not exists pgcrypto;
+
+update user_data
+set password = crypt(password, gen_salt('bf', 8));
